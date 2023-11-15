2e4799ee807c02de70aab384aaadf780aaa85212
AuniShah <radennn.auni@gmail.com>
2023-11-15 15:36:22 +0800

    updated app.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,26 +1,28 @@
-//import React from "react";
+import React from "react";
 
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-}
+ const Hello = (props) => {
+   console.log(props)
+   return (
+     <div>
+       <p>Hello {props.name}, you are {props.age} years old</p>
+     </div>
+   )
+ }
 
-const App = () => {
-  const name = "Peter";
+ const App = () => {
+   const name = "Peter";
   const age = 10;
-  return (
-    <div>
-      <h1>Greetings</h1>
+   return (
+     <div>
+       <h1>Greetings</h1>
 
-      <Hello name = "Maya" age = {26 + 10} />
-      <Hello name = {name} age = {age}/>
-    </div>
-  )
-}
+       <Hello name = "Maya" age = {26 + 10} />
+       <Hello name = {name} age = {age}/>
+     </div>
+   )
+ }
+
+
 
 
 export default App //without this , the whole app breaks down
